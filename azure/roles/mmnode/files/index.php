20e3dd995a1eb7e58fecb7de1bfa18495056a223
Rens Vlemmings <rens.vlemmings@live.nl>
2020-01-25 12:44:36 +0100

    Changed index.php

--- azure/roles/mmnode/files/index.php
+++ azure/roles/mmnode/files/index.php
@@ -1,13 +1,14 @@
 <?php
-echo '<h4> Status webserver 1 </h4>';
-echo str_replace("\n","<BR>",file_get_contents('webserver1.txt'));
-echo '-----<BR>';
-echo '<h4> Status webserver 2 </h4>';
-echo str_replace("\n","<BR>",file_get_contents('webserver2.txt'));
+echo '<h4> Status webservers </h4>';
+echo str_replace("\n","<BR>",file_get_contents('webservers.txt'));
 echo '-----<BR>';
 echo '<h4> Status Data server </h4>';
 echo str_replace("\n","<BR>",file_get_contents('dataserver.txt'));
 echo '-----<BR>';
+echo '<h4> Status Backup server </h4>';
+echo str_replace("\n","<BR>",file_get_contents('backupserver.txt'));
+echo '-----<BR>';
 echo '<h4> Status SQL server </h4>';
 echo str_replace("\n","<BR>",file_get_contents('sqlserver.txt'));
-echo '<p> Contenct wordt elke 2 minuten geupdated </p>';
+echo '-----<BR>';
+echo '<p> Status wordt elke 2 minuten geupdated </p>';
